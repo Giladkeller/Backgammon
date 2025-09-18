f2e3df374a1879100bc5c1a227e454f76b1b9c74
Keller6738 <itaykeller12345@gmail.com>
2025-09-18 13:57:05 +0300

    conect between sign in login add dialogs for errors and do that name from login/sign in go to users

--- app/src/main/java/com/example/backgammonfinal/OpenActivity.java
+++ app/src/main/java/com/example/backgammonfinal/OpenActivity.java
@@ -1,11 +1,13 @@
 package com.example.backgammonfinal;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,15 +20,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class OpenActivity extends AppCompatActivity {
 
+
+    private AlertDialog.Builder builder;
     Intent intent;
 
-    private Dialog dialog;
+    private Dialog loginDialog, signinDialog,dialog;
 
     private EditText editDUsername, editDEmail, editDPassword;
 
-    private TextView tvDMessage;
+    private TextView tvDMessage, btnNewAccount;
 
     private Button btnDGoBack;
+
+    private String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,55 +51,98 @@ public class OpenActivity extends AppCompatActivity {
     }
 
     public void createSignInDialog() {
-        dialog = new Dialog(this);
+        signinDialog = new Dialog(this);
 
-        dialog.setContentView(R.layout.signin);
+        signinDialog.setContentView(R.layout.signin);
 
-        dialog.setTitle("please register to my app");
+        signinDialog.setTitle("please register to my app");
 
-        dialog.setCancelable(true);
+        signinDialog.setCancelable(true);
 
-        tvDMessage = (TextView) dialog.findViewById(R.id.tvSignin);
-        editDUsername = (EditText) dialog.findViewById(R.id.edUsername);
-        editDEmail = (EditText) dialog.findViewById(R.id.edEmail);
-        editDPassword = (EditText) dialog.findViewById(R.id.edPassword);
-        btnDGoBack = (Button) dialog.findViewById(R.id.btnRegister);
+        tvDMessage = (TextView) signinDialog.findViewById(R.id.tvSignin);
+        editDUsername = (EditText) signinDialog.findViewById(R.id.edUsername);
+        editDEmail = (EditText) signinDialog.findViewById(R.id.edEmail);
+        editDPassword = (EditText) signinDialog.findViewById(R.id.edPassword);
+        btnDGoBack = (Button) signinDialog.findViewById(R.id.btnRegister);
 
         btnDGoBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(OpenActivity.this, MainActivity.class);
+                userName = editDUsername.getText().toString().trim();
+                intent = new Intent(OpenActivity.this, users.class);
+                intent.putExtra("USERNAME_KEY" , userName);
 
-                startActivity(intent);
+                if (editDUsername.getText().toString().isEmpty()||editDPassword.getText().toString().isEmpty()||editDEmail.getText().toString().isEmpty()){
+                    builder = new AlertDialog.Builder(OpenActivity.this);
+                    builder.setTitle("Error");
+                    builder.setMessage("UserName, Password or Email Null");
+                    builder.setPositiveButton("ok", (dialog, which) -> {
+                        dialog.dismiss();
+                    });
+                    builder.setNegativeButton("I have account!", (dialog , which) -> {
+                        dialog.dismiss();
+                        signinDialog.dismiss();
+                        createLoginDialog();
+                    });
+                    builder.show();
+                }
+                else
+                    startActivity(intent);
             }
         });
 
-        dialog.show();
+        signinDialog.show();
     }
 
     public void createLoginDialog() {
-        dialog = new Dialog(this);
+        loginDialog = new Dialog(this);
 
-        dialog.setContentView(R.layout.login);
+        loginDialog.setContentView(R.layout.login);
 
-        dialog.setTitle("please Login to my app");
+        loginDialog.setTitle("please Login to my app");
 
-        dialog.setCancelable(true);
+        loginDialog.setCancelable(true);
 
-        tvDMessage = (TextView) dialog.findViewById(R.id.tvLogin);
-        editDUsername = (EditText) dialog.findViewById(R.id.edUsername);
-        editDPassword = (EditText) dialog.findViewById(R.id.edPassword);
-        btnDGoBack = (Button) dialog.findViewById(R.id.btnLogin);
+        tvDMessage = (TextView) loginDialog.findViewById(R.id.tvLogin);
+        btnNewAccount = (TextView) loginDialog.findViewById(R.id.btnNewAccount);
+        editDUsername = (EditText) loginDialog.findViewById(R.id.edUsername);
+        editDPassword = (EditText) loginDialog.findViewById(R.id.edPassword);
+        btnDGoBack = (Button) loginDialog.findViewById(R.id.btnLogin);
 
+        btnNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginDialog.dismiss();
+                createSignInDialog();
+            }
+        });
         btnDGoBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(OpenActivity.this, MainActivity.class);
 
-                startActivity(intent);
+                userName = editDUsername.getText().toString().trim();
+                intent = new Intent(OpenActivity.this, users.class);
+                intent.putExtra("USERNAME_KEY" , userName);
+
+                if (editDUsername.getText().toString().isEmpty()||editDPassword.getText().toString().isEmpty()){
+                    builder = new AlertDialog.Builder(OpenActivity.this);
+                    builder.setTitle("Error");
+                    builder.setMessage("UserName or Password Null");
+                    builder.setPositiveButton("ok", (dialog, which) -> {
+                        dialog.dismiss();
+                    });
+                    builder.setNegativeButton("New User?", (dialog , which) -> {
+                        dialog.dismiss();
+                        loginDialog.dismiss();
+                       createSignInDialog();
+                    });
+                    builder.show();
+                }
+                else
+                    startActivity(intent);
             }
         });
 
-        dialog.show();
+        loginDialog.show();
     }
 }
