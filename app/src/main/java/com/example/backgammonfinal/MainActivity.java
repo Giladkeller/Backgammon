e1a7c20e569fe72bfbbc2039121e65605207cb08
Keller6738 <itaykeller12345@gmail.com>
2025-09-28 19:50:24 +0300

    create java for Main

--- app/src/main/java/com/example/backgammonfinal/MainActivity.java
+++ app/src/main/java/com/example/backgammonfinal/MainActivity.java
@@ -1,22 +1,15 @@
 package com.example.backgammonfinal;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.viewpager2.widget.ViewPager2;
 
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
-
-public class MainActivity extends AppCompatActivity {
-
-    private TabLayout tabLayout;
-    private ViewPager2 viewPager;
-    private TabLayoutMediator mediator;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,32 +21,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        tabLayout = findViewById(R.id.tab_layout);
-        viewPager = findViewById(R.id.view_pager);
-
-        viewPager.setAdapter(new MainPagerAdapter(this));
-        viewPager.setOffscreenPageLimit(3);
-
-        mediator = new TabLayoutMediator(tabLayout, viewPager,
-                new TabLayoutMediator.TabConfigurationStrategy() {
-                    @Override
-                    public void onConfigureTab(TabLayout.Tab tab, int position) {
-                        switch (position) {
-                            case 0: tab.setText("Roles"); break;
-                            case 1: tab.setText("GAME"); break;
-                            case 2: tab.setText("Chat"); break;
-                        }
-                        // אופציונלי: אייקונים
-                        // if (position == 0) tab.setIcon(R.drawable.ic_home);
-                    }
-                });
-        mediator.attach();
     }
 
     @Override
-    protected void onDestroy() {
-        if (mediator != null) mediator.detach();
-        super.onDestroy();
+    public void onClick(View view) {
+
     }
 }
